824e408419e97403382ebaec868e7061828fbf5c
Jessica Lord <jlord@github.com>
2016-07-02 20:19:48 -0700

    Update keys'

--- assets/js/assess-data.js
+++ assets/js/assess-data.js
@@ -1,22 +1,23 @@
-var data = require('../data.json')
 var fs = require('fs')
+
+var data = require('../data.json')
 
 var stats = {}
 
 writeStats()
 
 function writeStats() {
-  getVisitedCountries(data)
-  getVisitedMusuems(data)
+  getCountries(data)
+  getMusuems(data)
   getVisible(data)
   getSeen(data)
   fs.writeFileSync('assets/stats.json', 'var stats = ' + JSON.stringify(stats, null, ' '))
 }
 
-function getVisitedCountries (data) {
+function getCountries (data) {
   var visited = []
   var all = []
-  var delta = []
+  var remaining = []
   data.forEach(function (visit) {
     if (visit.Country === '') return
     if (all.indexOf(visit.Country) < 0) {
@@ -26,43 +27,42 @@ function getVisitedCountries (data) {
       visited.push(visit.Country)
     }
     all.forEach(function (country) {
-      if (visited.indexOf(country) < 0) delta.push(country)
+      if (visited.indexOf(country) < 0) remaining.push(country)
     })
   })
   stats.visitedCountries = visited
   stats.visitedCountriesNo = visited.length
   stats.allCountries = all
   stats.allCountriesNo = all.length
-  stats.CountriesDelta = delta
+  stats.CountriesRemaining = remaining
 }
 
-function getVisitedMusuems (data) {
+function getMusuems (data) {
   var visited = []
   var all = []
-  var delta = []
+  var remaining = []
   data.forEach(function (visit) {
-    if (visit.Location === '') return
-    if (all.indexOf(visit.Location) < 0) {
-      all.push(visit.Location)
+    if (visit['Permanent Location'] === '') return
+    if (all.indexOf(visit['Permanent Location']) < 0) {
+      all.push(visit['Permanent Location'])
     }
     if (visit.Seen === 'Y' && visited.indexOf(visit.Location) < 0) {
-      visited.push(visit.Location)
+      visited.push(visit['Permanent Location'])
     } else return
   })
   all.forEach(function (museum) {
     if (museum.match('Isabella') || museum.match('Private')) return
-    if (visited.indexOf(museum) < 0) delta.push(museum)
+    if (visited.indexOf(museum) < 0) remaining.push(museum)
   })
   stats.visitedMuseums = visited
   stats.visitedMuseumsNo = visited.length
   stats.allMuseums = all
   stats.allMuseumsNo = all.length
-  stats.museumDelta = delta
-  console.log(stats.museumDelta)
+  stats.museumRemaining = remaining
 }
 
 function getVisible (data) {
-  var all = data.map(function (piece) { return piece.Name})
+  var all = data.map(function (piece) { return piece.Name })
   var visible = []
   var notPubic = []
   data.forEach(function (piece) {
@@ -79,13 +79,13 @@ function getVisible (data) {
 
 function getSeen (data) {
   var seen = []
-  var unseen = []
+  var remaining = []
   data.forEach(function (piece) {
     if (piece.Seen === 'Y') seen.push(piece.Name)
-    else unseen.push(piece.Name)
+    else remaining.push(piece.Name)
   })
   stats.seen = seen
   stats.seenNo = seen.length
-  stats.unseen = unseen
-  stats.unseenNo = unseen.length - 2 // because not public
+  stats.remaining = remaining
+  stats.reminingNo = remaining.length - 2 // because not public
 }
