2a62b267439c5a94ba0af4c03d2d7e7f760ec4f6
Jessica Lord <jess.lord@gmail.com>
2018-10-05 21:10:37 -0400

    Update data generator

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,16 +3,19 @@ var Tabletop = require('tabletop')
 
 init()
 
+// 1PX5_V64YhlXWTtFpGPsUWow1wfwzTF1gh5benBXz8vw
+
 function init() {
   Tabletop.init({
     key: '1PX5_V64YhlXWTtFpGPsUWow1wfwzTF1gh5benBXz8vw',
-    callback: saveData,
-    simpleSheet: true
+    callback: saveData
   })
 }
 
 function saveData(data, tabletop) {
-  var formattedBlooleans = JSON.stringify(data, null, ' ')
+  var data = tabletop.sheets("Paintings").elements
+  console.log("Writing " + data.length + " items")
+  var formattedBlooleans = JSON.stringify(data, " ", null)
     .replace(new RegExp('"true"', 'g'), true)
     .replace(new RegExp('"false"', 'g'), false)
   var vermeers = 'var vermeers = ' + formattedBlooleans
